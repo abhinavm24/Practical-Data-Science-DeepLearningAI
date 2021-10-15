0f5800f6e2eee9dd8bbfebe5ac085931714af722
Abhinav Misra <{ID}+{username}@users.noreply.github.com>
2021-10-16 01:33:34 +0530

    c2w1 review

--- course 2/c2w1/src/prepare_data.py
+++ course 2/c2w1/src/prepare_data.py
@@ -185,7 +185,7 @@ def convert_to_bert_input_ids(review, max_seq_length):
           review,
           add_special_tokens=True,
           ### BEGIN SOLUTION - DO NOT delete this comment for grading purposes
-          max_length=None, # Replace None
+          max_length=max_seq_length, # Replace None
           ### END SOLUTION - DO NOT delete this comment for grading purposes
           return_token_type_ids=False,
           padding='max_length',
@@ -403,6 +403,7 @@ def _preprocess_file(file,
         except:
             pass
         print('Offline store status: {}'.format(offline_store_status))    
+        sleep(15)
     print('...features ingested!')
 
 
